6aae4d9301762903e1909ede22decf9f23b5ccf6
Мария Твёрдая <93594972+Mariarty007@users.noreply.github.com>
2022-05-10 13:38:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 8000;
+        int oneMile = 20;
+
+       
+
+        System.out.println(("За билет будет начислено : ") + (ticket / oneMile) + (" миль"));
+
+    }
+}
+
+
