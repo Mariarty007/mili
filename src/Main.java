3068f2c6bf2399f0a2ac928d48679c73cda9af68
Мария Твёрдая <93594972+Mariarty007@users.noreply.github.com>
2022-05-10 13:54:26 +0300

    Miles

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
         int ticket = 8000;
         int oneMile = 20;
 
-       
-
         System.out.println(("За билет будет начислено : ") + (ticket / oneMile) + (" миль"));
 
     }
 }
 
+//C:\Users\User\.jdks\corretto-11.0.15\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1\lib\idea_rt.jar=53221:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\User\IdeaProjects\miles\out\production\miles Main
+        //За билет будет начислено : 400 миль//
 
+        //Process finished with exit code 0//
